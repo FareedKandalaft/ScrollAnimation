23ecc947acb2bf4d13b3963a12369077d524d360
Fareed-TI <fkandalaft@tradeinformatics.com>
2021-03-29 16:18:32 -0400

    Initialized and Completed

---
+++ script.js
@@ -0,0 +1,21 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  // how was the formula determined??? 
+  const triggerBottom = window.innerHeight / 5 * 4;
+
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop <triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  })
+
+};
